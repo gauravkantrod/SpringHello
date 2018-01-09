366b6b0e4db0b6a9f94d9aab293c5487876e4fe5
Gaurav Kantrod <gaurav.kantrod@vodafone.com>
2018-01-09 12:25:19 +0530

    tenth commit

--- src/main/java/com/mkyong/common/App.java
+++ src/main/java/com/mkyong/common/App.java
@@ -16,6 +16,8 @@ public class App
     	
     	
     	
+    	
+    	
     	ApplicationContext context = new ClassPathXmlApplicationContext("Spring-Module.xml");
     	 
         HelloWorld obj = (HelloWorld) context.getBean("helloBean");
